05eed1a57c9ce42052790e8244b3fd8f0b27ba57
Pablo Vial <pvial@chicagobooth.edu>
2019-01-29 16:44:24 +0000

    Safe

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 AWS.config(
-  :access_key_id => "AKIAINAZUAKXLFJMFP5A",
-  :secret_access_key => "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"
+  :access_key_id => "AKIAIP4UGMMLQATMKIWQ",
+  :secret_access_key => "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"
 )
 
 S3_BUCKET =  AWS::S3.new.buckets["repositorio-portal"]
